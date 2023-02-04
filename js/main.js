3ad496cba00a50197913b2a46e16b9d08aec5f70
wagtrung26 <nqt99design@gmail.com>
2023-02-05 03:56:26 +0700

    d

--- js/main.js
+++ js/main.js
@@ -2892,3 +2892,12 @@ function more() {
     moreText.style.display = "block";
   }
 }
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
