e5d930ff832ff7413423ef8bd70596f8da7b831d
wagtrung26 <nqt99design@gmail.com>
2023-02-06 04:10:28 +0700

    d

---
+++ js/main2.js
@@ -0,0 +1,44 @@
+
+jQuery(document).ready(function($) {
+
+	"use strict";
+
+	
+
+
+  var siteIstotope = function() {
+  	/* activate jquery isotope */
+	  var $container = $('#posts').isotope({
+	    itemSelector : '.item',
+	    isFitWidth: true
+	  });
+
+	  $(window).resize(function(){
+	    $container.isotope({
+	      columnWidth: '.col-sm-3'
+	    });
+	  });
+	  
+	  $container.isotope({ filter: '*' });
+
+	    // filter items on button click
+	  $('#filters').on( 'click', 'button', function() {
+	    var filterValue = $(this).attr('data-filter');
+	    $container.isotope({ filter: filterValue });
+	    $('#filters button').removeClass('active');
+	    $(this).addClass('active');
+	  });
+  }
+
+  siteIstotope();
+
+
+  $('.fancybox').on('click', function() {
+	  var visibleLinks = $('.fancybox');
+
+	  $.fancybox.open( visibleLinks, {}, visibleLinks.index( this ) );
+
+	  return false;
+	});
+
+});
