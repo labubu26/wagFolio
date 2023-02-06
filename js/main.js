6d4e8393985d5f736549452743131e365a7fcd5f
wagtrung26 <nqt99design@gmail.com>
2023-02-06 15:21:12 +0700

    yu

--- js/main.js
+++ js/main.js
@@ -2747,56 +2747,86 @@
     }
 
 
-    var cate = document.getElementsByClassName(".cate");
-    if (!cate) return;
-    else {
-      filterSelection("all")
+    // var cate = document.getElementsByClassName(".cate");
+    // if (!cate) return;
+    // else {
+    //   filterSelection("all")
 
-      function filterSelection(c) {
-        var x, i;
-        x = document.getElementsByClassName("column");
-        if (c == "all") c = "";
-        for (i = 0; i < x.length; i++) {
-          w3RemoveClass(x[i], "show");
-          if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-        }
-      }
+    //   function filterSelection(c) {
+    //     var x, i;
+    //     x = document.getElementsByClassName("column");
+    //     if (c == "all") c = "";
+    //     for (i = 0; i < x.length; i++) {
+    //       w3RemoveClass(x[i], "show");
+    //       if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    //     }
+    //   }
 
-      function w3AddClass(element, name) {
-        var i, arr1, arr2;
-        arr1 = element.className.split(" ");
-        arr2 = name.split(" ");
-        for (i = 0; i < arr2.length; i++) {
-          if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
-        }
-      }
+    //   function w3AddClass(element, name) {
+    //     var i, arr1, arr2;
+    //     arr1 = element.className.split(" ");
+    //     arr2 = name.split(" ");
+    //     for (i = 0; i < arr2.length; i++) {
+    //       if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
+    //     }
+    //   }
 
-      function w3RemoveClass(element, name) {
-        var i, arr1, arr2;
-        arr1 = element.className.split(" ");
-        arr2 = name.split(" ");
-        for (i = 0; i < arr2.length; i++) {
-          while (arr1.indexOf(arr2[i]) > -1) {
-            arr1.splice(arr1.indexOf(arr2[i]), 1);
-          }
-        }
-        element.className = arr1.join(" ");
-      }
+    //   function w3RemoveClass(element, name) {
+    //     var i, arr1, arr2;
+    //     arr1 = element.className.split(" ");
+    //     arr2 = name.split(" ");
+    //     for (i = 0; i < arr2.length; i++) {
+    //       while (arr1.indexOf(arr2[i]) > -1) {
+    //         arr1.splice(arr1.indexOf(arr2[i]), 1);
+    //       }
+    //     }
+    //     element.className = arr1.join(" ");
+    //   }
 
-      var btnContainer = document.getElementById("myBtnContainer");
-      var btns = btnContainer?.getElementsByClassName("btn");
-      for (var i = 0; i < btns?.length; i++) {
-        btns[i].addEventListener("click", function () {
-          var current = document.getElementsByClassName("active");
-          current[0].className = current[0].className.replace(" active", "");
-          this.className += " active";
-        });
-      }
+    //   var btnContainer = document.getElementById("myBtnContainer");
+    //   var btns = btnContainer?.getElementsByClassName("btn");
+    //   for (var i = 0; i < btns?.length; i++) {
+    //     btns[i].addEventListener("click", function () {
+    //       var current = document.getElementsByClassName("active");
+    //       current[0].className = current[0].className.replace(" active", "");
+    //       this.className += " active";
+    //     });
+    //   }
 
-    }
+    // }
 
     
-    //
+    // cate filter
+
+    var siteIstotope = function() {
+      /* activate jquery isotope */
+      var $container = $('#posts').isotope({
+        itemSelector : '.item',
+        isFitWidth: true
+      });
+  
+      $(window).resize(function(){
+        $container.isotope({
+          columnWidth: '.col-sm-3'
+        });
+      });
+      
+      $container.isotope({ filter: '*' });
+  
+        // filter items on button click
+      $('#filters').on( 'click', 'button', function() {
+        var filterValue = $(this).attr('data-filter');
+        $container.isotope({ filter: filterValue });
+        $('#filters button').removeClass('active');
+        $(this).addClass('active');
+      });
+    }
+  
+    siteIstotope();
+   
+   
+   
+  //  lax
     var lax =document.querySelector(".lax")
     if (lax) {
       gsap
@@ -2893,48 +2923,48 @@
 
 })();
 
+// cate
 
+// function filterSelection(c) {
+//   var x, i;
+//   x = document.getElementsByClassName("column");
+//   if (c == "all") c = "";
+//   for (i = 0; i < x.length; i++) {
+//     w3RemoveClass(x[i], "show");
+//     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+//   }
+// }
 
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("column");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
+// function w3AddClass(element, name) {
+//   var i, arr1, arr2;
+//   arr1 = element.className.split(" ");
+//   arr2 = name.split(" ");
+//   for (i = 0; i < arr2.length; i++) {
+//     if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
+//   }
+// }
 
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
-  }
-}
+// function w3RemoveClass(element, name) {
+//   var i, arr1, arr2;
+//   arr1 = element.className.split(" ");
+//   arr2 = name.split(" ");
+//   for (i = 0; i < arr2.length; i++) {
+//     while (arr1.indexOf(arr2[i]) > -1) {
+//       arr1.splice(arr1.indexOf(arr2[i]), 1);
+//     }
+//   }
+//   element.className = arr1.join(" ");
+// }
 
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer?.getElementsByClassName("btn");
-for (var i = 0; i < btns?.length; i++) {
-  btns[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
+// var btnContainer = document.getElementById("myBtnContainer");
+// var btns = btnContainer?.getElementsByClassName("btn");
+// for (var i = 0; i < btns?.length; i++) {
+//   btns[i].addEventListener("click", function () {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
 
 
 
