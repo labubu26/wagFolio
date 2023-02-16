5770b819a7c55c93994386741ce0b012ea80e33e
wagtrung26 <nqt99design@gmail.com>
2023-02-16 17:53:20 +0700

    hjj

--- js/main.js
+++ js/main.js
@@ -3127,18 +3127,18 @@ function myFunction() {
 
 // block
 
-document.addEventListener("keydown", function (event){
-  if (event.ctrlKey){
-     event.preventDefault();
-  }
-  if(event.keyCode == 123){
-     event.preventDefault();
-  }
-});
+// document.addEventListener("keydown", function (event){
+//   if (event.ctrlKey){
+//      event.preventDefault();
+//   }
+//   if(event.keyCode == 123){
+//      event.preventDefault();
+//   }
+// });
 
-document.addEventListener("contextmenu",
-  event => event.preventDefault()
-  );
+// document.addEventListener("contextmenu",
+//   event => event.preventDefault()
+//   );
 
   
 
