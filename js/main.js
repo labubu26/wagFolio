373a5ff0050d2eb8c271e9fb3f13d0052fd4449f
wagtrung26 <nqt99design@gmail.com>
2023-02-21 22:06:29 +0700

    h

--- js/main.js
+++ js/main.js
@@ -3153,16 +3153,16 @@ function myFunction() {
 
 // block
 
-// document.addEventListener("keydown", function (event){
-//   if (event.ctrlKey){
-//      event.preventDefault();
-//   }
-//   if(event.keyCode == 123){
-//      event.preventDefault();
-//   }
-// });
+document.addEventListener("keydown", function (event){
+  if (event.ctrlKey){
+     event.preventDefault();
+  }
+  if(event.keyCode == 123){
+     event.preventDefault();
+  }
+});
 
-// document.addEventListener("contextmenu",
-//   event => event.preventDefault()
-//   );
+document.addEventListener("contextmenu",
+  event => event.preventDefault()
+  );
 
