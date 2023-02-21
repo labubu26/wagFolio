93b9c8b2c8445c3f168f877b112660a9b1162f39
wagtrung26 <nqt99design@gmail.com>
2023-02-22 01:05:31 +0700

    đ

--- js/main.js
+++ js/main.js
@@ -3153,16 +3153,32 @@ function myFunction() {
 
 // block
 
-document.addEventListener("keydown", function (event){
-  if (event.ctrlKey){
-     event.preventDefault();
-  }
-  if(event.keyCode == 123){
-     event.preventDefault();
-  }
-});
+// document.addEventListener("keydown", function (event){
+//   if (event.ctrlKey){
+//      event.preventDefault();
+//   }
+//   if(event.keyCode == 123){
+//      event.preventDefault();
+//   }
+// });
 
-document.addEventListener("contextmenu",
-  event => event.preventDefault()
-  );
+// document.addEventListener("contextmenu",
+//   event => event.preventDefault()
+//   );
 
+
+  const link = encodeURI(window.location.href);
+const msg = encodeURIComponent('Hey, I found this article');
+const title = encodeURIComponent('Article or Post Title Here');
+
+const fb = document.querySelector('.facebook');
+fb.href = `https://www.facebook.com/share.php?u=${link}`;
+
+const linkedIn = document.querySelector('.linkedin');
+linkedIn.href = `https://www.linkedin.com/sharing/share-offsite/?url=${link}`;
+
+// const reddit = document.querySelector('.reddit');
+// reddit.href = `http://www.reddit.com/submit?url=${link}&title=${title}`;
+
+const whatsapp = document.querySelector('.whatsapp');
+whatsapp.href = `https://api.whatsapp.com/send?text=${msg}: ${link}`;
