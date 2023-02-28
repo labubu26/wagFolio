1a3293a3e40875fb97fbe4660ef01ad87e691c85
wagtrung26 <nqt99design@gmail.com>
2023-02-28 18:37:29 +0700

    s

--- js/main.js
+++ js/main.js
@@ -3082,6 +3082,8 @@
 
     $('img[data-enlargable]').addClass('img-enlargable').click(function(){
       var src = $(this).attr('src');
+    console.log(" something1 ", src)
+
       $('<div>').css({
           background: 'RGBA(0,0,0,.7) url('+src+') no-repeat center',
           backgroundSize: 'contain',
@@ -3095,22 +3097,23 @@
       }).appendTo('body');
   });
 
-//   $('div[data-enlargable]').addClass('img-enlargable').click(function(){
-//     var it = $(this);
-//     var src = it.attr('data-background');
+  $('div[data-enlargable]').addClass('img-enlargable').click(function(){
+    var it = $(this);
+    var src = it.attr('data-enlargable');
+    console.log(" something2 ", $(this))
    
-//     $('<div>').css({
-//         background: 'RGBA(0,0,0,.7) url('+src+') no-repeat center',
-//         backgroundSize: 'contain',
-//         width:'100%', height:'100%',
-//         position:'fixed',
-//         zIndex:'10000',
-//         top:'0', left:'0',
-//         cursor: 'zoom-out'
-//     }).click(function(){
-//         $(this).remove();
-//     }).appendTo('body');
-// });
+    $('<div>').css({
+        background: 'RGBA(0,0,0,.7) url('+src+') no-repeat center',
+        backgroundSize: 'contain',
+        width:'100%', height:'100%',
+        position:'fixed',
+        zIndex:'10000',
+        top:'0', left:'0',
+        cursor: 'zoom-out'
+    }).click(function(){
+        $(this).remove();
+    }).appendTo('body');
+});
 
 
     // img-compare
