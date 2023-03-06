b08f9c16496b112a28f8cbbb4d34ea4b56c7921a
wagtrung26 <nqt99design@gmail.com>
2023-03-07 01:59:43 +0700

    d

--- js/main.js
+++ js/main.js
@@ -3391,16 +3391,16 @@ function beforeAfter() {
 
 // block
 
-// document.addEventListener("keydown", function (event){
-//   if (event.ctrlKey){
-//      event.preventDefault();
-//   }
-//   if(event.keyCode == 123){
-//      event.preventDefault();
-//   }
-// });
+document.addEventListener("keydown", function (event){
+  if (event.ctrlKey){
+     event.preventDefault();
+  }
+  if(event.keyCode == 123){
+     event.preventDefault();
+  }
+});
 
-// document.addEventListener("contextmenu",
-//   event => event.preventDefault()
-//   );
+document.addEventListener("contextmenu",
+  event => event.preventDefault()
+  );
 
