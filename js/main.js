86b06342d110d5686b2076457005efa5dc86c52c
wagtrung26 <nqt99design@gmail.com>
2023-04-12 22:17:42 +0700

    dd

--- js/main.js
+++ js/main.js
@@ -3460,16 +3460,16 @@ $('[href=#]').click(function(){return false});
 
 // block
 
-// document.addEventListener("keydown", function (event){
-//   if (event.ctrlKey){
-//      event.preventDefault();
-//   }
-//   if(event.keyCode == 123){
-//      event.preventDefault();
-//   }
-// });
+document.addEventListener("keydown", function (event){
+  if (event.ctrlKey){
+     event.preventDefault();
+  }
+  if(event.keyCode == 123){
+     event.preventDefault();
+  }
+});
 
-// document.addEventListener("contextmenu",
-//   event => event.preventDefault()
-//   );
+document.addEventListener("contextmenu",
+  event => event.preventDefault()
+  );
 
