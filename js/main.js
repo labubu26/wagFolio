931d8c2f9e98fc3977dd315616659f587c180079
waggithub99 <trungnq@inspirelab.io>
2024-04-25 14:38:36 +0700

    fgdfg

--- js/main.js
+++ js/main.js
@@ -3818,15 +3818,15 @@ function stagerhover() {
 
 // block
 
-// document.addEventListener("keydown", function (event){
-//   if (event.ctrlKey){
-//      event.preventDefault();
-//   }
-//   if(event.keyCode == 123){
-//      event.preventDefault();
-//   }
-// });
+document.addEventListener("keydown", function (event){
+  if (event.ctrlKey){
+     event.preventDefault();
+  }
+  if(event.keyCode == 123){
+     event.preventDefault();
+  }
+});
 
-// document.addEventListener("contextmenu",
-//   event => event.preventDefault()
-//   );
+document.addEventListener("contextmenu",
+  event => event.preventDefault()
+  );
