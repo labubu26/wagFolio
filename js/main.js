84ebde447365511a6c74e7f009797d05f3a54464
wagpro <nqt99design@gmail.com>
2024-07-18 18:12:47 +0700

    dfsdf

--- js/main.js
+++ js/main.js
@@ -3877,15 +3877,15 @@ function stagerhover() {
 
 // block
 
-document.addEventListener("keydown", function (event){
-  if (event.ctrlKey){
-     event.preventDefault();
-  }
-  if(event.keyCode == 123){
-     event.preventDefault();
-  }
-});
+// document.addEventListener("keydown", function (event){
+//   if (event.ctrlKey){
+//      event.preventDefault();
+//   }
+//   if(event.keyCode == 123){
+//      event.preventDefault();
+//   }
+// });
 
-document.addEventListener("contextmenu",
-  event => event.preventDefault()
-  );
+// document.addEventListener("contextmenu",
+//   event => event.preventDefault()
+//   );
