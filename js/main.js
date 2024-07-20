63914dcdf22fcbd75ea0ad45df108fa3d14590c3
wagpro <nqt99design@gmail.com>
2024-07-21 01:03:05 +0700

    đ

--- js/main.js
+++ js/main.js
@@ -3352,37 +3352,37 @@ import modalUnlock from "./module/modalunlock.js";
   }
   // End a()
 
-  // duyệt tất cả tấm ảnh cần lazy-load
-  const lazyImages = document.querySelectorAll('[lazy]');
+  // // duyệt tất cả tấm ảnh cần lazy-load
+  // const lazyImages = document.querySelectorAll('[lazy]');
 
-  // chờ các tấm ảnh này xuất hiện trên màn hình
-  const lazyImageObserver = new IntersectionObserver((entries, observer) => {
-    entries.forEach((entry) => {
-      // tấm ảnh này đã xuất hiện trên màn hình
-      if (entry.isIntersecting) {
-        const lazyImage = entry.target;
-        const src = lazyImage.dataset.src;
+  // // chờ các tấm ảnh này xuất hiện trên màn hình
+  // const lazyImageObserver = new IntersectionObserver((entries, observer) => {
+  //   entries.forEach((entry) => {
+  //     // tấm ảnh này đã xuất hiện trên màn hình
+  //     if (entry.isIntersecting) {
+  //       const lazyImage = entry.target;
+  //       const src = lazyImage.dataset.src;
 
-        lazyImage.tagName.toLowerCase() === 'img'
-          // <img>: copy data-src sang src
-          ? lazyImage.src = src
+  //       lazyImage.tagName.toLowerCase() === 'img'
+  //         // <img>: copy data-src sang src
+  //         ? lazyImage.src = src
 
-          // <div>: copy data-src sang background-image
-          : lazyImage.style.backgroundImage = "url(\'" + src + "\')";
+  //         // <div>: copy data-src sang background-image
+  //         : lazyImage.style.backgroundImage = "url(\'" + src + "\')";
 
-        // copy xong rồi thì bỏ attribute lazy đi
-        lazyImage.removeAttribute('lazy');
+  //       // copy xong rồi thì bỏ attribute lazy đi
+  //       lazyImage.removeAttribute('lazy');
 
-        // job done, không cần observe nó nữa
-        observer.unobserve(lazyImage);
-      }
-    });
-  });
+  //       // job done, không cần observe nó nữa
+  //       observer.unobserve(lazyImage);
+  //     }
+  //   });
+  // });
 
-  // Observe từng tấm ảnh và chờ nó xuất hiện trên màn hình
-  lazyImages.forEach((lazyImage) => {
-    lazyImageObserver.observe(lazyImage);
-  });
+  // // Observe từng tấm ảnh và chờ nó xuất hiện trên màn hình
+  // lazyImages.forEach((lazyImage) => {
+  //   lazyImageObserver.observe(lazyImage);
+  // });
 
 
 
@@ -3887,15 +3887,15 @@ function stagerhover() {
 
 // block
 
-document.addEventListener("keydown", function (event){
-  if (event.ctrlKey){
-     event.preventDefault();
-  }
-  if(event.keyCode == 123){
-     event.preventDefault();
-  }
-});
+// document.addEventListener("keydown", function (event){
+//   if (event.ctrlKey){
+//      event.preventDefault();
+//   }
+//   if(event.keyCode == 123){
+//      event.preventDefault();
+//   }
+// });
 
-document.addEventListener("contextmenu",
-  event => event.preventDefault()
-  );
+// document.addEventListener("contextmenu",
+//   event => event.preventDefault()
+//   );
