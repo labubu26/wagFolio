fa95483ab6e81836e0c6b70271edcc22dbf21f3a
wagpro <nqt99design@gmail.com>
2024-08-28 11:19:16 +0700

    cc

--- js/main.js
+++ js/main.js
@@ -3923,15 +3923,15 @@ function stagerhover() {
 
 // block
 
-document.addEventListener("keydown", function (event){
-  if (event.ctrlKey){
-     event.preventDefault();
-  }
-  if(event.keyCode == 123){
-     event.preventDefault();
-  }
-});
+// document.addEventListener("keydown", function (event){
+//   if (event.ctrlKey){
+//      event.preventDefault();
+//   }
+//   if(event.keyCode == 123){
+//      event.preventDefault();
+//   }
+// });
 
-document.addEventListener("contextmenu",
-  event => event.preventDefault()
-  );
+// document.addEventListener("contextmenu",
+//   event => event.preventDefault()
+//   );
